98e1ad04d572d62ca5cc6e3180d8615c99c191e9
Pedro Jorge <pedro.paulo@gubee.com.br>
2021-08-25 18:39:44 -0300

    Commit 3 put for noticia/id was added where it sends the specified noticia to all emails saved

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const storage = require('node-persist');
 const bodyParser = require('body-parser');
-
+const nodemailer = require('nodemailer');
 const app = express();
 app.use(bodyParser.json({ extended: true }));
 
@@ -78,6 +78,56 @@ app.post('/inscricao', async (req, res) => {
     res.status(200)
     res.send({email: req.body.email})
 });
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'gerardo.morissette55@ethereal.email',
+        pass: 'FnpnWTS3MBred9pE9f'
+    }
+});
+
+app.put('/noticia/:id', async (req, res) => {
+    let foundResult = await getNoticia(req)
+    if (foundResult === undefined) {
+        res.status(404)
+        res.send()
+    } else {
+        let emailList = undefined;
+        await storage.init();
+        let emailArray;
+        try{
+            emailArray =  await storage.getItem('email')
+        }catch {
+            emailArray = undefined
+        }
+        if(emailArray === undefined){
+            res.status(404)
+            res.send("No emails saved")
+        }else{
+            let emailText = "Titulo: " + foundResult.titulo + "\n\n" +
+                "Url: " + foundResult.url + "\n\n" +
+                "Resumo: " + foundResult.resumo
+            emailArray.forEach((email)=>{
+                if(emailList === undefined){
+                    emailList = email.email;
+                }else{
+                    emailList += ", " + email.email
+                }
+            })
+            let info = await transporter.sendMail({
+                from: '"Gerardo Morissette" <gerardo.morissette55@ethereal.email>', // sender address
+                to: emailList, // list of receivers
+                subject: foundResult.titulo, // Subject line
+                text: emailText, // plain text body
+            });
+            console.log(nodemailer.getTestMessageUrl(info))
+        }
+        res.status(200)
+        res.send(emailList)
+    }
+})
 
 
 
